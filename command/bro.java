fb6b605bd1b3c6c111a65d9cc596ae8e9fd459b9
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-12 14:43:08 +0530

    hai you

--- command/bro.java
+++ command/bro.java
@@ -1,4 +1,8 @@
 public class bro{
+    /*
+        1)bro file
+        2)using variables
+    */
     public static void main(String args[]){
         int X=10; //this is int value
         int Y=20; //this is int value
