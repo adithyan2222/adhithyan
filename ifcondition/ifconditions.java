4f6f1dce65b32e040be89291781ce70e0550da65
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 10:58:12 +0530

    for

---
+++ ifcondition/ifconditions.java
@@ -0,0 +1,11 @@
+public class ifcondition{
+
+    public static void main(String args[]){
+        if(false){
+            System.out.println("she is a lady");
+        }
+        if(true){
+            System.out.println("she is not a lady");
+        }
+    }
+}
