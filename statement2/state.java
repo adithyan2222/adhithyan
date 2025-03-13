1eda53bf7a68a0e3dbd46b98802ab430942c9859
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 11:40:37 +0530

    mrs

---
+++ statement2/state.java
@@ -0,0 +1,10 @@
+public class statement2{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("i am a good boy");
+        }
+        else{
+            System.out.println("he is not");
+      }
+    }
+}
