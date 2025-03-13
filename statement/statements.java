6b5ea6fee5c7361dcf411ec267cd7396624f4eb4
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 11:11:14 +0530

    mr

---
+++ statement/statements.java
@@ -0,0 +1,10 @@
+public class statements{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("i am a good person");
+        }
+        else{
+            System.out.println("he is not");
+      }
+    }
+}
