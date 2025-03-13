692202f08ae66afd42925f234ec2e911b9db4a70
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 11:44:41 +0530

    sir

---
+++ arithmetics2/arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetics2{
+    Arithmetics2 Operator;
+    public static void main(String args[]){
+        System.out.println(33+2);
+        System.out.println(10-5);
+        System.out.println(10*5);
+        System.out.println(16/4);
+        System.out.println(10%4);
+    }
+}
