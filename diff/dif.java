e8aefdb8ce6901c7ab8f5e0108f3136da83a1594
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 11:56:50 +0530

    hello sir

---
+++ diff/dif.java
@@ -0,0 +1,12 @@
+public class diff{
+    public static void main(String args[]){
+        int x = 20;
+        if(x<=50){
+            System.out.println("minor");
+        
+                    }
+                    else{
+                        System.out.println("major");
+                    }
+    }
+}
