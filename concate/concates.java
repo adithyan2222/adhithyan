58027bf9c866f9e7a026207675cda31a6493c4b7
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 13:49:46 +0530

    boss

---
+++ concate/concates.java
@@ -0,0 +1,11 @@
+public class concates {
+        void concat(){
+            String x = "bob";
+            String y = "paul";
+            System.out.println(x+y);
+        }
+    public static void main(String args[]){
+         concates bye = new concates();
+            bye.concat();
+    }
+}
