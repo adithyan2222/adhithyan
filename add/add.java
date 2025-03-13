4467c35668f8812e9d0e274750afab100114f7ef
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-13 15:47:45 +0530

    messages

---
+++ add/add.java
@@ -0,0 +1,10 @@
+public class add{
+    void add(int a, int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String args[]){
+        add van = new add();
+        van.add(33,55);
+     }
+}
