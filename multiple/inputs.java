4b318b298d17d88b6df26c5a66af0d316c9713bb
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 15:11:13 +0530

    going

---
+++ multiple/inputs.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MultipleInput {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n = input.nextInt();
+        
+        int a[] = new int[n];
+        int i = 0;
+        while(i < n) {
+            System.out.println("Enter the value:");
+        }
+    }
+}
+
+    
+       
