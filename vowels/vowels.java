2402b58d30f379e57d15b09635d7165a561cd3cd
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 11:39:56 +0530

    simples

---
+++ vowels/vowels.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class vowels{
+    public static void main(String args[]){
+        String v[] = {"a","e","i","o","u"};
+Scanner input = new Scanner(System.in);
+String alpha = input.nextLine();
+int detect = 0;
+int i = 0;
+while(i<=4){
+    if(alpha.equals(v[i])){
+        detect = 1;
+    }
+    i=i+1;
+    }
+    if(detect==1){
+        System.out.println("vowels");
+    }
+    else{
+System.out.println("not vowels");
+    }
+    }
+}
