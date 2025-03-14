c36b92a937201c552b7411b3ddac20a7960b96aa
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 13:32:54 +0530

    going

---
+++ spliting/split.java
@@ -0,0 +1,9 @@
+public class split{
+public split(){
+}
+        public static void main(String args[]){
+            String var1 = "anaconda,python";
+            String[] var2 = var1.split(",");
+            System.out.println(var2[0]);
+    }
+}
