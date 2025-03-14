a9f18ceae5bad8ed209352b0dc0153a58cc9162e
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 10:12:44 +0530

    simple

---
+++ file/while.java
@@ -0,0 +1,11 @@
+ package file;
+ import java.util.*;
+public class file{
+    public static void main(String args[]){
+    int i = 0;
+    while(i<7){
+ System.out.println(i);
+ i=i+1;
+    }
+    }
+}
