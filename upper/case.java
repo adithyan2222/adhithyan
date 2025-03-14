06fad7a2a4c39c3fa872bfcfe23c251d60936e4b
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 12:10:39 +0530

    simples

---
+++ upper/case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class upper{
+    public static void main(String args[]){
+        String name = "anaconda";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+}
+}
