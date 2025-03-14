9f6bef91b4b49021ba5316c0c347dc41bb9af24d
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-14 10:30:40 +0530

    simples

---
+++ n/number.java
@@ -0,0 +1,14 @@
+package n;
+import java.util.*;
+public class n{
+    public static void main(String args[]){
+Scanner obj = new Scanner(System.in);
+System.out.println("enter n");
+int a = obj.nextInt();
+int i=1;
+while(i<=a){
+    System.out.println(i);
+    i++;
+      }
+    }
+}
