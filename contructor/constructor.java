597e06c837ace127f43f4d42a8a73671605118f0
adithyan2222 <adithyanaajimon@gmail.com>
2025-03-15 10:40:12 +0530

    haii

---
+++ contructor/constructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class constructor{
+    constructor(){
+        System.out.println("day by");
+    }
+    
+        public static void main(String args[]){
+
+        }
+    }
---
+++ prime/prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class num{
+}
+public static void main(String args[]){
+        }
+ Scanner Scanner  = new Scanner(System.in)
+System.out.println("enter the number");
+int number = Scanner.nextInt();
+if(isprime(number)){
+System.out.println(number +"is prime number. ");
+}
+else{
+}
+System.out.println(number +"not prime number. ");
+Scanner.close();
+public static boolean isprime(int num){
+    if( num<=1){
+        return false;
+    }
+
+}
+   
+
+        
+
